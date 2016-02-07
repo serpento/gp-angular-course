24513f4b7fc498a96881005a558c96e810724ddf
serpento <simsonne@tut.by>
2016-02-07 21:30:15 +0300

    HW-2 Synchronize values in inputs and url (extra task)

--- app/js/sum.controller.js
+++ app/js/sum.controller.js
@@ -1,10 +1,14 @@
 angular.module("sumModule", ['ngRoute'])
 
-    .controller('sumCtrl', function($scope, $routeParams) {
+    .controller('sumCtrl', function($scope, $routeParams, $location) {
         $scope.list = {
             summand1: parseInt($routeParams.summand1) || '',
             summand2: parseInt($routeParams.summand2) || ''
         };
+
+        $scope.setUrl = function() {
+            $location.path('/' + $scope.list.summand1 + '/' + $scope.list.summand2);
+        }
     })
 
     .directive('laSum', function() {
